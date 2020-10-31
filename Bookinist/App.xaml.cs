5d5f377ceb704940ba949a1df369f5647855b492
Infarh <shmachilin@yandex.ru>
2020-10-31 18:22:25 +0300

    Контейнер сервисов

--- Bookinist/App.xaml.cs
+++ Bookinist/App.xaml.cs
@@ -1,8 +1,36 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Bookinist
 {
     public partial class App : Application
     {
+        private static IHost __Host;
+
+        public static IHost Host => __Host
+            ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
---
+++ Bookinist/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace Bookinist
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+           .CreateDefaultBuilder(args)
+           .ConfigureServices(App.ConfigureServices);
+    }
+}
